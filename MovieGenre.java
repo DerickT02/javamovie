9e72d7c191751c0b00ca20c14944855b280e41da
asanti689 <aaronsantillana@csus.edu>
2024-11-10 23:41:53 -0800

    Update MovieGenre.java

--- MovieGenre.java
+++ MovieGenre.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class MovieGenre implements Comparator<Movie> {
+public class MovieGenre implements Comparator<Movie> { //Used to compare based on genre
     public int compare(Movie a, Movie b){
         return a.getGenre().compareTo(b.getGenre());
     }
