9846deaf2ba84296cf3ee01e496b48ebc243f2ea
asanti689 <aaronsantillana@csus.edu>
2024-11-10 23:41:35 -0800

    Update MovieReleaseDate.java

--- MovieReleaseDate.java
+++ MovieReleaseDate.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class MovieReleaseDate implements Comparator<Movie> {
+public class MovieReleaseDate implements Comparator<Movie> { //Used to compare based on release date
     public int compare(Movie a, Movie b){
         return a.getReleaseDate().compareTo(b.getReleaseDate());
     }
